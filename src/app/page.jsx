d18c55cfb520fbc0370bade782724f0cde09a737
Jiayu You <a1847115@student.adelaide.edu.au>
2024-04-08 23:59:15 +0930

    find multiple records in database

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,18 +15,24 @@ async function run() {
     // Get the database and collection on which to run the operation
     const database = client.db("sample_mflix");
     const movies = database.collection("movies");
-    // Query for a movie that has the title 'The Room'
-    const query = { title: "The Room" };
+    // Query for movies that have a runtime less than 15 minutes
+    const query = { runtime: { $lt: 15 } };
     const options = {
-      // Sort matched documents in descending order by rating
-      sort: { "imdb.rating": -1 },
-      // Include only the `title` and `imdb` fields in the returned document
+      // Sort returned documents in ascending order by title (A->Z)
+      sort: { title: 1 },
+      // Include only the `title` and `imdb` fields in each returned document
       projection: { _id: 0, title: 1, imdb: 1 },
     };
-    // Execute query
-    const movie = await movies.findOne(query, options);
-    // Print the document returned by findOne()
-    console.log(movie);
+    // Execute query 
+    const cursor = movies.find(query, options);
+    // Print a message if no documents were found
+    if ((await movies.countDocuments(query)) === 0) {
+      console.log("No documents found!");
+    }
+    // Print returned documents
+    for await (const doc of cursor) {
+      console.dir(doc);
+    }
   } finally {
     await client.close();
   }
