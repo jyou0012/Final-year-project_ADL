7bf826ab50fccdac94cb0fc9f7786d5953c02cb3
Jiayu You <a1847115@student.adelaide.edu.au>
2024-04-08 23:51:53 +0930

    find single document

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,38 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { MongoClient } from 'mongodb'
+//MONGODB_URI="mongodb+srv://a1847115:Jiayu@cluster0.oneyoul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+const client = new MongoClient("mongodb+srv://a1847115:Jiayu@cluster0.oneyoul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+await client.connect();
+
+
+
+
+ 
+async function run() {
+  try {
+    
+    // Get the database and collection on which to run the operation
+    const database = client.db("sample_mflix");
+    const movies = database.collection("movies");
+    // Query for a movie that has the title 'The Room'
+    const query = { title: "The Room" };
+    const options = {
+      // Sort matched documents in descending order by rating
+      sort: { "imdb.rating": -1 },
+      // Include only the `title` and `imdb` fields in the returned document
+      projection: { _id: 0, title: 1, imdb: 1 },
+    };
+    // Execute query
+    const movie = await movies.findOne(query, options);
+    // Print the document returned by findOne()
+    console.log(movie);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 export default function Page() {
-  return <p>TODO</p>
+  return <p>TODO hello</p>
 }
