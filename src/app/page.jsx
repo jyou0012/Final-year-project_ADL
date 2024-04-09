af78c206a898cfa791f62439ccdb43962010ba0a
Jiayu You <a1847115@student.adelaide.edu.au>
2024-04-09 21:30:55 +0930

    change monogoDB URL to local environment variable

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,9 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { MongoClient } from 'mongodb'
 //MONGODB_URI="mongodb+srv://a1847115:Jiayu@cluster0.oneyoul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-const client = new MongoClient("mongodb+srv://a1847115:Jiayu@cluster0.oneyoul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+//const client = new MongoClient("mongodb+srv://a1847115:Jiayu@cluster0.oneyoul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+const client = new MongoClient(process.env.MONGODB_URI);
 await client.connect();
 
 
@@ -17,8 +19,12 @@ await client.connect();
     // Create an array of documents to insert
     const docs = [
       { name: "a18111111", Day: "Monday" , Date: "2024-4-1", Task:"Learn MonongoDB"},
-      { name: "a18111112", Day: "Monday" , Date: "2024-4-2"},
-      { name: "a18111113", Day: "Monday" , Date: "2024-4-3", Task:"Learn MonongoDB3" }
+      { name: "a18111112", Day: "Tuesday" , Date: "2024-4-2"},
+      { name: "a18111113", Day: "Wednesday" , Date: "2024-4-1", Task:"Learn MonongoDB"},
+      { name: "a18111112", Day: "Thursday" , Date: "2024-4-5", Task:"Learn MonongoDB"},
+      { name: "a18111115", Day: "Friday" , Date: "2024-4-6", Task:"Learn MonongoDB"},
+      { name: "a18111112", Day: "Saturday" , Date: "2024-4-7", Task:"Learn MonongoDB"},
+      { name: "a18111116", Day: "Sunday" , Date: "2024-4-3", Task:"Learn MonongoDB3" }
     ];
     // Prevent additional documents from being inserted if one fails
     const options = { ordered: true };
@@ -32,6 +38,7 @@ await client.connect();
   }
 }
 run().catch(console.dir); */
+
 // update
 /* async function run() {
   try {
@@ -56,26 +63,26 @@ run().catch(console.dir); */
     await client.close();
   }
 }
-run().catch(console.dir); */
+run().catch(console.dir);  */
 
 // delete
-async function run() {
-  try {
-    const database = client.db("MCI2024");
-    const timesheet = database.collection("timesheet");
-    /* Delete all documents that match the specified regular
-    expression in the title field from the "timesheet" collection */
-    const query = { Task: { $regex: "After" } };
-    const result = await timesheet.deleteMany(query);
-    // Print the number of deleted documents
-    console.log("Deleted " + result.deletedCount + " documents");
-  } finally {
-    // Close the connection after the operation completes
-    await client.close();
-  }
-}
-// Run the program and print any thrown exceptions
-run().catch(console.dir);
+ async function run() {
+    try {
+      const database = client.db("MCI2024");
+      const timesheet = database.collection("timesheet");
+      /* Delete all documents that match the specified regular
+      expression in the title field from the "timesheet" collection */
+      const query = { Task: { $regex: "After" } };
+      const result = await timesheet.deleteMany(query);
+      // Print the number of deleted documents
+      console.log("Deleted " + result.deletedCount + " documents");
+    } finally {
+      // Close the connection after the operation completes
+      await client.close();
+    }
+ }
+ // Run the program and print any thrown exceptions
+ run().catch(console.dir);
 
 export default function Page() {
   return <p>TODO hello</p>
