bafbe45e46eca170628904a2e3c08ab80b3e9aa6
Jiayu You <a1847115@student.adelaide.edu.au>
2024-04-21 23:50:36 +0930

    change on timesheet.js

--- src/database/timesheet.js
+++ src/database/timesheet.js
@@ -4,3 +4,19 @@ export default function timesheetData() {
     Mon2: 2,
   };
 }
+
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+
+
+export async function timesheetData(week) {
+  await client.connect();
+  const db = client.db("TimesheetDashboard");
+  const timesheet = db.collection("timesheet");
+
+  const r = await timesheet.findOne({ week: 1 });
+  console.log(r);
+  return r;
+}
