cad317140f499ed25347f60281dcc832cba760a1
Jiayu You <a1847115@student.adelaide.edu.au>
2024-04-21 23:26:15 +0930

    add code for database

--- src/app/students/actions.jsx
+++ src/app/students/actions.jsx
@@ -4,6 +4,8 @@ import { MongoClient } from "mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
+
+
 export async function timesheetData(week) {
   await client.connect();
   const db = client.db("TimesheetDashboard");
@@ -16,17 +18,56 @@ export async function timesheetData(week) {
 }
 
 export default async function timesheetFormAction(formData) {
+  //const client = new MongoClient(process.env.MONGODB_URI);
+  console.log(formData);
   await client.connect();
   const db = client.db("TimesheetDashboard");
-  await db.createCollection("timesheet");
+  //await db.createCollection("timesheet");
   const timesheet = db.collection("timesheet");
   await timesheet.updateOne(
     { week: formData.get("Week") },
     {
       $set: {
-        week: formData.get("Week"),
+        week: formData.get("week"),
         mon: {
+          date: formData.get("MonDate"),
+          timeStart: formData.get("MonStart"),
+          timeEnd: formData.get("MonEnd"),
           task: formData.get("MonTask"),
+          fit : formData.get("MonFit"),
+          outcome : formData.get("MonOutcome")
+        },
+        tue: {
+          date: formData.get("TueDate"),
+          timeStart: formData.get("TueStart"),
+          timeEnd: formData.get("TueEnd"),
+          task: formData.get("TueTask"),
+          fit : formData.get("TueFit"),
+          outcome : formData.get("TueOutcome")
+        },
+        wed: {
+          date: formData.get("WedDate"),
+          timeStart: formData.get("WedStart"),
+          timeEnd: formData.get("WedEnd"),
+          task: formData.get("WedTask"),
+          fit : formData.get("WedFit"),
+          outcome : formData.get("WedOutcome")
+        },
+        thu: {
+          date: formData.get("ThuDate"),
+          timeStart: formData.get("ThuStart"),
+          timeEnd: formData.get("ThuEnd"),
+          task: formData.get("ThuTask"),
+          fit : formData.get("ThuFit"),
+          outcome : formData.get("ThuOutcome")
+        },
+        fri: {
+          date: formData.get("FriDate"),
+          timeStart: formData.get("FriStart"),
+          timeEnd: formData.get("FriEnd"),
+          task: formData.get("FriTask"),
+          fit : formData.get("FriFit"),
+          outcome : formData.get("FriOutcome")
         },
       },
     },
