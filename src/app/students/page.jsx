79f666b0af785ffa6a5bf02bf6ae25264a54be95
Jiayu You <a1847115@student.adelaide.edu.au>
2024-04-28 20:03:29 +0930

    commit 28/4

--- src/app/students/page.jsx
+++ src/app/students/page.jsx
@@ -1,6 +1,6 @@
 import TimesheetFormTabs from "../../components/TimesheetFormTabs";
 import  timesheetFormAction  from './actions';
-import { dbTimesheetGetByStudent,dbTimesheetGetByStudentDraft } from "../../database/timesheet";
+import { dbTimesheetGetByStudent,processTimesheets } from "../../database/timesheet";
 
 
 export default async function Layout() {
@@ -10,9 +10,9 @@ export default async function Layout() {
       action={timesheetFormAction}
       dataWeeks={JSON.parse(
         JSON.stringify(
-          await dbTimesheetGetByStudent({
-            student: "a1234567"            
-          }),
+          await processTimesheets(
+             {student: "a1234567"}            
+          ),
         ),
       )}
     />
