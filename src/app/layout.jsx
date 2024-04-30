cc79ccf8c67cf6e9cd9eb663ef8dfea39e740240
Xinrun Wu <xinrun.wu@student.adelaide.edu.au>
2024-04-30 20:03:56 +0930

    Adjust body

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,10 @@
+import Box from "@mui/material/Box";
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body style={{ margin: "0px", padding: "0px" }}>
+      <Box component="body" sx={{ margin: "0px", padding: "0px" }}>
         {/* Layout UI */}
         {children}
-      </body>
-    </html>
+      </Box>
   );
 }
