be836b662f6663c09cfbcb521eb9a3cfc68a6e28
Xinrun Wu <xinrun.wu@student.adelaide.edu.au>
2024-05-02 07:39:18 +0930

    Set theme

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,22 @@
+"use client";
 import Box from "@mui/material/Box";
+import { blue } from "@mui/material/colors";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[700],
+    },
+  },
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Box component="body" sx={{ margin: "0px", padding: "0px" }}>
         {/* Layout UI */}
-        {children}
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>;
       </Box>
     </html>
   );
