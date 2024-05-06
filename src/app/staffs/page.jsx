c8b51ea4489f7a71153490185f24c4f64a2b0256
Shiyao Wu <shiyao.wu@student.adelaide.edu.au>
2024-05-06 19:43:31 +0800

    import scv Shiyao_import_info and total hours

--- src/app/staffs/page.jsx
+++ src/app/staffs/page.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import Box from "@mui/material/Box";
 import WeekOverviewTable from "../../components/WeekOverviewTable";
+import CsvUploadButton from '../../components/CsvUploadButton'; // Ensure the path is correct
 
 export default function ConditionView() {
   return (
     <Box sx={{ p: 2 }}>
+      <CsvUploadButton />
       <WeekOverviewTable />
     </Box>
   );
