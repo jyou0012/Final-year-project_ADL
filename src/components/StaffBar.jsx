d56979df4cd8ba137a3f26fb4fcb2a8d66900d4e
Xinrun Wu <xinrun.wu@student.adelaide.edu.au>
2024-05-21 19:52:56 +0930

    JoJo: StaffBar & StaffdetailSel

---
+++ src/components/StaffBar.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import * as React from "react";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import Grid from "@mui/material/Grid";
+
+function handleClick(event, key) {
+  event.preventDefault();
+  console.info(`You clicked a breadcrumb with key: ${key}`);
+  // 这里你可以根据 key 的不同值执行不同的逻辑
+  switch (key) {
+    case "1":
+      console.info("Do something for JOJO1");
+      break;
+    case "2":
+      console.info("Do something for Draft Content");
+      break;
+    case "3":
+      console.info("Do something for Submit Content");
+      break;
+  }
+}
+export default function StaffBar() {
+  const breadcrumbs = [
+    <Link underline="hover" key="1" color="inherit" href="/">
+      Group detail
+    </Link>,
+    <Link
+      underline="hover"
+      key="2"
+      color="inherit"
+      href="http://localhost:3000/staffs"
+    >
+      Week detail
+    </Link>,
+    <Link
+      underline="hover"
+      key="3"
+      color="inherit"
+      href="http://localhost:3000/staffs/detail"
+    >
+      Student detail
+    </Link>,
+  ];
+
+  return (
+    <Grid container mx="3%" my="20px">
+      <Stack>
+        <Breadcrumbs separator="-" aria-label="breadcrumb">
+          {breadcrumbs}
+        </Breadcrumbs>
+      </Stack>
+    </Grid>
+  );
+}
---
+++ src/components/StaffdetailSel.jsx
@@ -0,0 +1,49 @@
+"use client";
+
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Grid from "@mui/material/Grid";
+
+export default function StaffdetailSel() {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <Grid container mx="3%" my="20px">
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
+        <Select
+          value={age}
+          onChange={handleChange}
+          displayEmpty
+          inputProps={{ "aria-label": "Without label" }}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Grid>
+  );
+}
